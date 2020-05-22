52c98cb13e104d3c251149ebaf1769d4f204754d
bradenchurch <56855817+bradenchurch@users.noreply.github.com>
2020-05-21 20:05:03 -0600

    Add files via upload

---
+++ bankroll.rb
@@ -0,0 +1,21 @@
+class Bankroll
+#  
+
+    attr_accessor :wallet
+    
+
+    def initialize(wallet)
+        @wallet = wallet
+    end
+
+    def display_wallet
+        #show how much money player has
+        puts wallet
+    end
+
+end
+
+# amount = Bankroll.new(0)
+# amount.display_wallet
+
+# p amount
